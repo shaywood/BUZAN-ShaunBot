5a529684d7ecc00a06e7860fdd6482f26fa2233d
Steven Haywood <steven123456789@hotmail.co.uk>
2011-09-25 12:51:01 +0100

    Add a (null) copyright notice, and credits to the file.
    At some point, I might even add some code!

---
+++ ShaunBot.py
@@ -0,0 +1,11 @@
+# This is ShaunBot, BUZAN's IRC bot.
+# This work is effectively public domain
+# because license enforcement isn't worth the fuss.
+# The Python IRC Bot framework used can be found at:
+# https://github.com/LukeusMaximus/Python-IRC-Bot-Framework.git
+# And full credit for that goes to Luke. :)
+
+# This file written by Steven Haywood, steven123456789@hotmail.co.uk
+
+# "Constants":
+
