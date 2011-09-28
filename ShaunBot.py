d0f8c602631c27d90f448db7a5286122ffcbfaf1
Steven Haywood <steven123456789@hotmail.co.uk>
2011-09-28 21:48:16 +0100

    Update params in ShaunBot.Log

--- ShaunBot.py
+++ ShaunBot.py
@@ -331,7 +331,7 @@ class ShaunBot:
 			self.Bot.say(Dest, Message)
 	
 	def Log(self, Sender, Dest, Message):
-		LogStr = asctime() + "<" + From + "> " + To + ": " + Message		
+		LogStr = asctime() + "<" + Sender + "> " + Dest + ": " + Message		
 		print LogStr
 
 		if self.LogFile != None:
